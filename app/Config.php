d20c32ec8b3cc943a5cf7bd5bb23ff6af6dc1e7d
Light <102204002+aztice@users.noreply.github.com>
2024-08-16 00:44:51 +0800

    更新 Config.php

--- app/Config.php
+++ app/Config.php
@@ -8,7 +8,8 @@ class Config{
             'title' => '轻雨 Auth',
             'description' => '由轻雨科技团队开发的账号系统，用于账号管理、授权等功能。',
             'keywords' => '账号管理,Auth,轻雨科技,账号系统,授权系统',
-            'FileCache' => false
+            'FileCache' => false,
+            'Template' => 'default'
         );
     }
 }
