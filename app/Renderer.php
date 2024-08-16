4fe1d68d2b3c18c4b38aa7525638a4da0d019f31
Light <102204002+aztice@users.noreply.github.com>
2024-08-16 14:38:32 +0800

    Update Renderer.php

--- app/Renderer.php
+++ app/Renderer.php
@@ -13,9 +13,13 @@ class Renderer {
     }
     public function load($actionName) {
         $path = '../templates/' . $this->config['Template'] . '/' . $actionName . '.html';
+        if(is_file($path)){}else{
+            die('Template: '.$actionName.' -> 模板不存在');
+        }
         $html = file_get_contents($path);
         $html = $this->render($html, 'config');
         $html = $this->render($html, 'assets');
+        $html = $this->render($html, 'phrases');
         echo $this->render($html, 'template');
     }
     
@@ -44,9 +48,9 @@ class Renderer {
                         $type = $matches[1];
                         $file = $matches[2];
                         if ($type === 'css') {
-                            return '<link rel="stylesheet" href="assets/css/' . $file . '.css' . $uri . '">';
+                            return '<link rel="stylesheet" href="/assets/css/' . $file . '.css' . $uri . '">';
                         } elseif ($type === 'js') {
-                            return '<script src="assets/js/' . $file . '.js' . $uri . '"></script>';
+                            return '<script src="/assets/js/' . $file . '.js' . $uri . '"></script>';
                         }
                     },
                     $template
@@ -60,9 +64,29 @@ class Renderer {
                         if (file_exists($path)) {
                             $data = file_get_contents($path);
                             $data = $this->render($data, 'config');
+                            $data = $this->render($data, 'phrases');
                             return $this->render($data, 'assets');
                         } else {
                             die("子模板 -> $path 不存在");
+                        }
+                    },
+                    $template
+                );
+            case 'phrases':
+                return preg_replace_callback(
+                    '/\${phrase\.(\w+)}/',
+                    function ($matches) {
+                        $phraseKey = $matches[1];
+                        $phrasePath = '../templates/' . $this->config['Template'] . '/phrases.json';
+                        if (file_exists($phrasePath)) {
+                            $phrases = json_decode(file_get_contents($phrasePath), true);
+                            if (isset($phrases[$phraseKey])) {
+                                return $phrases[$phraseKey];
+                            } else {
+                                die("phrases.json -> 关键字 $phraseKey 不存在");
+                            }
+                        } else {
+                            die("phrases.json 文件不存在");
                         }
                     },
                     $template
