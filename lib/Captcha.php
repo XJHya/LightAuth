f429308d43c999d91b06b0c0c2395c2f924f720a
Light <102204002+aztice@users.noreply.github.com>
2024-08-16 20:15:31 +0800

    Update Captcha.php

--- lib/Captcha.php
+++ lib/Captcha.php
@@ -1,32 +1,41 @@
 <?php
 namespace Lib;
 
-class Captcha{
-    public function LoadCaptcha(){
+class Captcha {
+    public function LoadCaptcha() {
         session_start();
+        $width = 100;
+        $height = 30;
+        $bgColor = [255, 255, 255, 30];
+        $image = imagecreatetruecolor($width, $height);
+        $backgroundColor = imagecolorallocatealpha($image, ...$bgColor);
+        imagefill($image, 0, 0, $backgroundColor);
+        $data = "asdfghjklqwertyuipzxcvbnm123456789";
         $code = "";
-        $image = imagecreatetruecolor(100, 30);
-        $color = imagecolorallocatealpha($image, 255, 255, 255, 30);
-        imagefill($image, 0, 0, $color);
-        for ($i = 0; $i < 4; $i++) {
-            $fontSize = 8;
+        $fontSize = 8;
+        $charCount = 4;
+        
+        for ($i = 0; $i < $charCount; $i++) {
             $fontColor = imagecolorallocate($image, rand(0, 120), rand(0, 120), rand(0, 120));
-            $data = "asdfghjklqwertyuipzxcvbnm123456789";
-            $fontContent = substr($data, rand(0, strlen($data) - 1), 1);
-            $code .= $fontContent;
-            $x = ($i * 100 / 4) + rand(0, 9);
+            $char = $data[rand(0, strlen($data) - 1)];
+            $code .= $char;
+            $x = ($i * $width / $charCount) + rand(0, 9);
             $y = rand(5, 10);
-            imagestring($image, $fontSize, $x, $y, $fontContent, $fontColor);
+            imagestring($image, $fontSize, $x, $y, $char, $fontColor);
         }
+        
         $_SESSION['code'] = $code;
-        for ($i = 0; $i < 200; $i++) {
+        $pointCount = 200;
+        for ($i = 0; $i < $pointCount; $i++) {
             $pointColor = imagecolorallocate($image, rand(100, 220), rand(100, 220), rand(100, 220));
-            imagesetpixel($image, rand(0, 98), rand(1, 29), $pointColor);
+            imagesetpixel($image, rand(0, $width - 1), rand(1, $height - 1), $pointColor);
         }
-        for ($i = 0; $i < 3; $i++) {
+        $lineCount = 3;
+        for ($i = 0; $i < $lineCount; $i++) {
             $lineColor = imagecolorallocate($image, rand(50, 150), rand(50, 150), rand(50, 150));
-            imageline($image, rand(1, 99), rand(1, 29), rand(1, 99), rand(1, 29), $lineColor);
+            imageline($image, rand(1, $width - 1), rand(1, $height - 1), rand(1, $width - 1), rand(1, $height - 1), $lineColor);
         }
+
         header('Content-type: image/png');
         imagepng($image);
         imagedestroy($image);
