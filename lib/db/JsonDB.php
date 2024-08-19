034e3191cea36c31a807378cfeb2cd1f581898ab
Light <102204002+aztice@users.noreply.github.com>
2024-08-19 21:13:33 +0800

    Update JsonDB.php

--- lib/db/JsonDB.php
+++ lib/db/JsonDB.php
@@ -1,4 +1,37 @@
 <?php
+namespace JsonDB;
+
+function CreateLock($dbname,$list){
+    $path = $_SERVER['DOCUMENT_ROOT'].'/db/'.$dbname.'/list/'.$list.'.lock';
+    touch($path);
+}
+function IsLock($dbname,$list){
+    $path = $_SERVER['DOCUMENT_ROOT'].'/db/'.$dbname.'/list/'.$list.'.lock';
+    if(file_exists($path)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function DeleteLock($dbname,$list){
+    $path = $_SERVER['DOCUMENT_ROOT'].'/db/'.$dbname.'/list/'.$list.'.lock';
+    unlink($path);
+}
+function backup($dbname){
+    if(!is_dir($_SERVER['DOCUMENT_ROOT'].'/Backup/')){
+        mkdir($_SERVER['DOCUMENT_ROOT'].'/Backup/');
+    }
+    $zip = new ZipArchive();
+    $zip->open($_SERVER['DOCUMENT_ROOT'].'/Backup/'.$dbname.'.jdb', ZipArchive::CREATE | ZipArchive::OVERWRITE);
+    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($_SERVER['DOCUMENT_ROOT'].'/db/'.$dbname.'/'), RecursiveIteratorIterator::LEAVES_ONLY);
+    foreach ($files as $name => $file) {
+        if (!$file->isDir()) {
+            $zip->addFile($file->getRealPath(), substr($file->getPathname(), strlen($folder_path) + 1));
+        }
+    }
+    $zip->close();
+}
 class jsonDB{
     public $dbname;
     public $config;
