e568adcdf94bdbb1832d53306b8dd9be8ac194bb
Light <102204002+aztice@users.noreply.github.com>
2024-08-19 21:13:53 +0800

    Update JsonDB.php

--- lib/db/inst/JsonDB.php
+++ lib/db/inst/JsonDB.php
@@ -4,18 +4,18 @@ Installation Application By Github aztice
 考虑到不同程序对于创建数据库等的兼容性，因此特地创建inst功能
 */  
 require_once('../lib/db/JsonDB.php');
+use JsonDB\JsonDB;
 class install{
-    public function __construct(){
-    }
-    public function init($db){
+    public function __construct($db){
         $jdb = new JsonDB();
         $jdb->createdb($db);
         $jdb->connect($db);
-        $jdb->CreateList('User');
+        $jdb->CreateList('UserData');
         $jdb->CreateList('TempCode');
         $jdb->CreateList('LoginData');
         $jdb->CreateList('AppList');
         $jdb->CreateList('LatestAC');
+        $jdb->CreateList('RateLimit');
         return true;
     }
 }
