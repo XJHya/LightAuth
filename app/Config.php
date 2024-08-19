a0914e63429579edee8a97c01639d3dbd26e1a07
Light <102204002+aztice@users.noreply.github.com>
2024-08-19 21:15:47 +0800

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -4,12 +4,10 @@ namespace App;
 class Config{
     public $config;
     public function __construct(){
-        $this->config = array(
-            'title' => '轻雨 Auth',
-            'description' => '由轻雨科技团队开发的账号系统，用于账号管理、授权等功能。',
-            'keywords' => '账号管理,Auth,轻雨科技,账号系统,授权系统',
-            'FileCache' => false,
-            'Template' => 'default'
-        );
+        $this->config = json_decode(file_get_contents(str_replace('/public','',$_SERVER['DOCUMENT_ROOT'].'/config.json')),true);
+    }
+    public function edit($key,$value){
+        $this->config[$key] = $value;
+        file_put_contents(str_replace('/public','',$_SERVER['DOCUMENT_ROOT'].'/config.json'),json_encode($this->config, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
     }
 }
